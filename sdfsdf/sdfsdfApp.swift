a1af26e7a99f520f0b846d4dce771bb13103aee4
user <user@pollub.edu.pl>
2024-04-27 08:17:00 +0200

    Initial Commit

---
+++ sdfsdf/sdfsdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdfsdfApp.swift
+//  sdfsdf
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct sdfsdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
